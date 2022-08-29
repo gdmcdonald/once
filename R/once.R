dd26644f75bee5e9bc32d8a562829a77dd42e9b3
gdmcdonald <gordon27@gmail.com>
2022-08-29 14:27:53 +1000

    add once

---
+++ R/once.R
@@ -0,0 +1,59 @@
+#' Execute Expensive Operations Only Once
+#'
+#' The `once()` function allows you to easily execute expensive compute operations only once, and save the resulting object to disk.
+#'
+#' @param expr The expensive expression to evaluate
+#' @param file_path File path for saving the output object as an Rds file. Defaults to an auto-generated file in the 'saved_objects' folder in current project's directory. Note that if no filename is provided it will rerun and re-save every time!
+#' @param rerun Rerun the expression anyway and save the result? Defaults to false.
+#'
+#' @return the results of expr
+#' @export
+#'
+#' @examples
+#' my_out <-
+#'   runif(1e8) %>% # some expensive operation
+#'   mean() %>%
+#'   once(file_path = "saved_random_number.Rds") # only do it once, save output to this file.
+once <- function(expr,
+                 file_path = NULL,
+                 rerun = FALSE){
+
+  #if there's no file path, use the current datetime
+  if (is.null(file_path)) {
+
+    datetime_string <- as.character(format(Sys.time(),
+                                           "%Y_%m_%d_T_%H_%M_%S"))
+    file_path <- here::here("saved_objects",
+                                   paste0("once_",
+                                          datetime_string,
+                                          ".Rds"))
+
+  }
+
+  if (file.exists(file_path) & !rerun){
+    # if the file exists and we don't want to re-run the code,
+    # load the object from the file.
+
+    output <- readRDS(file = file_path)
+
+  } else {
+
+    # Create the directory if it doesn't exist
+    output_dir <- dirname(file_path)
+
+    if (!dir.exists(output_dir)) {
+      dir.create(output_dir,
+                 recursive = T)
+    }
+
+    # Run the expensive operation
+    output <-
+      eval(expr = expr)
+
+    #Save the output to disk
+    saveRDS(output, file = file_path)
+
+  }
+  # Return the object
+  return(output)
+}
